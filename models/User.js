11475ed195e108c4701b77c968d4fd1ed6ae5ed3
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-06-24 21:59:16 +0300

    Add mongoose validation for User scheme

--- models/User.js
+++ models/User.js
@@ -6,15 +6,28 @@ const userSchema = new Schema(
     {
         firstName: {
             type: String,
+            maxlength: 50,
+            minlength: 2,
+            trim: true,
             required: true,
         },
         secondName: {
             type: String,
+            maxlength: 50,
+            minlength: 2,
+            trim: true,
             required: true,
         },
         phoneNumber: {
             type: String,
+            maxlength: 13,
+            minlength: 10,
             required: true,
+            trim: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
         },
     },
     { versionKey: false }
