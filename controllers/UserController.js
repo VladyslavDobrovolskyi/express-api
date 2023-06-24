56638b3ff2e3d1d61a2ef149f47f0784c0708d0b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-06-24 17:14:54 +0300

    Extend controller [GET PUT DELETE]  /:id endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,56 @@
 const User = require('../models/User')
 
-const getUsers = (req, res) => {
-    User.find()
-        .then((users) => {
-            res.status(200).json(users)
-        })
-        .catch((error) => console.log(error))
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find().exec()
+        res.status(200).json(users)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Ошибка сервера' })
+    }
+}
+const getUserById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id)
+        if (!user) {
+            return res.status(404).json({ message: 'Пользователь не найден' })
+        }
+        res.json(user)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Ошибка сервера' })
+    }
+}
+const editUserById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const updates = req.body
+        const options = { new: true }
+
+        const updatedUser = await User.findByIdAndUpdate(id, updates, options)
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'Пользователь не найден' })
+        }
+        res.json(updatedUser)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Ошибка сервера' })
+    }
+}
+const deleteUserById = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const deletedUser = await User.findByIdAndDelete(id)
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'Пользователь не найден' })
+        }
+        res.json({ message: 'Пользователь удален' })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Ошибка сервера' })
+    }
 }
 
 const createUser = async (req, res) => {
@@ -25,5 +70,8 @@ const createUser = async (req, res) => {
 
 module.exports = {
     getUsers,
+    getUserById,
+    editUserById,
+    deleteUserById,
     createUser,
 }
