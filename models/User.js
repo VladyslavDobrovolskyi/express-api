e0bd3ec458f5358c552bdf46a5f1eb8696b8281e
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-06-24 17:15:37 +0300

    Remove versionKey in  "User" model

--- models/User.js
+++ models/User.js
@@ -2,20 +2,23 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        secondName: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
     },
-    secondName: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-})
+    { versionKey: false }
+)
 
 const User = mongoose.model('User', userSchema)
 
--- routes/User.js
+++ routes/User.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { getUsers, createUser } = require('../controllers/UserController')
+const {
+    getUsers,
+    getUserById,
+    editUserById,
+    deleteUserById,
+    createUser,
+} = require('../controllers/UserController')
 
 router.get('/', getUsers)
+router.get('/:id', getUserById)
+router.put('/:id', editUserById)
+router.delete('/:id', deleteUserById)
 router.post('/', createUser)
 
 module.exports = router
