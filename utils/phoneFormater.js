2b94ace15884c4721d70add7185fc682e57223ad
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-06-26 16:14:21 +0300

    Fix phoneFormater.js

--- utils/phoneFormater.js
+++ utils/phoneFormater.js
@@ -3,7 +3,7 @@ module.exports = (phoneNumber) => {
         phoneNumber = '+38' + phoneNumber
     } else if (/^\380\d{9}$/.test(phoneNumber)) {
         phoneNumber = '+' + phoneNumber
-    } else if (/^\+38\d{9}$/.test(phoneNumber)) {
+    } else if (/^\+380\d{9}$/.test(phoneNumber)) {
         return phoneNumber
     } else return false
 
