7ac2507229f2a2c4851850181402623a1b514937
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-06-26 17:57:44 +0300

    Change errors and messages and edit server.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,22 +6,24 @@ const getUsers = async (req, res) => {
         res.status(200).json(users)
     } catch (error) {
         console.error(error)
-        res.status(500).json({ message: 'Ошибка сервера' })
+        res.status(500).json({ message: 'Internal Server Error.' })
     }
 }
+
 const getUserById = async (req, res) => {
     try {
         const id = req.params.id
         const user = await User.findById(id)
         if (!user) {
-            return res.status(404).json({ message: 'Пользователь не найден' })
+            return res.status(404).json({ message: 'User not found.' })
         }
         res.json(user)
     } catch (error) {
         console.error(error)
-        res.status(500).json({ message: 'Ошибка сервера' })
+        res.status(500).json({ message: 'Internal Server Error.' })
     }
 }
+
 const editUserById = async (req, res) => {
     try {
         const id = req.params.id
@@ -30,41 +32,52 @@ const editUserById = async (req, res) => {
 
         const updatedUser = await User.findByIdAndUpdate(id, updates, options)
         if (!updatedUser) {
-            return res.status(404).json({ message: 'Пользователь не найден' })
+            return res.status(404).json({ message: 'User not found.' })
         }
         res.json(updatedUser)
     } catch (error) {
         console.error(error)
-        res.status(500).json({ message: 'Ошибка сервера' })
+        res.status(500).json({ message: 'Internal Server Error.' })
     }
 }
+
 const deleteUserById = async (req, res) => {
     try {
         const id = req.params.id
 
         const deletedUser = await User.findByIdAndDelete(id)
         if (!deletedUser) {
-            return res.status(404).json({ message: 'Пользователь не найден' })
+            return res.status(404).json({ message: 'User not found.' })
         }
-        res.json({ message: 'Пользователь удален' })
+        res.json({ message: 'User deleted' })
     } catch (error) {
         console.error(error)
-        res.status(500).json({ message: 'Ошибка сервера' })
+        res.status(500).json({ error: 'Internal Server Error.' })
     }
 }
 
 const createUser = async (req, res) => {
+    const existingUser = await User.findOne({
+        emailAddress: req.body.emailAddress,
+    })
+    if (existingUser) {
+        return res
+            .status(400)
+            .json({ error: 'User with this email already exists.' })
+    }
+
     const user = new User({
         firstName: req.body.firstName,
         secondName: req.body.secondName,
         phoneNumber: req.body.phoneNumber,
+        emailAddress: req.body.emailAddress,
     })
 
     try {
         const savedUser = await user.save()
         res.status(201).json(savedUser)
     } catch (err) {
-        res.status(400).json({ error: err })
+        res.status(400).json({ error: err.message })
     }
 }
 
--- server.js
+++ server.js
@@ -2,30 +2,26 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
+
+const Router = require('./routes/User')
 
 const morgan = require('morgan')
 const helmet = require('helmet')
 const mongoose = require('mongoose')
 const sanitizer = require('express-mongo-sanitize')
 
-const Router = require('./routes/User')
-
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3000
-// eslint-disable-next-line no-undef
-const DB_URL = process.env.DB_URL
-
 mongoose
-    .connect(DB_URL)
+    .connect(process.env.DB_URL)
     .then(() => console.log(`Database: Successfully connected`))
     .catch((err) => console.log(`Database: Connection ERROR (${err.message})`))
 
-// eslint-disable-next-line no-undef
 app.use(morgan(process.env.LOG_LEVEL))
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(sanitizer())
+
 app.use('/users', Router)
 
 app.get('/', (req, res) => {
